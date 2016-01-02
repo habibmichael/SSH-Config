442678cfef58e6ac6d46183eb4d2c6fcdcffd39c
root <root@localhost.localdomain>
2016-01-02 10:23:29 -0800

    Check IP reachability

--- ssh_config.py
+++ ssh_config.py
@@ -31,6 +31,7 @@ def ip_is_valid():
 		#Octet Check 
 		for ip in ip_list:
 			octet_check_ip=ip.split('.')
+			print octet_check_ip
 			#Unicast address check, exclude reserved ip addresses
 			if((len(octet_check_ip)==4) and (1<=int(octet_check_ip[0])<=223) and (int(octet_check_ip[0])!=127) and (int(octet_check_ip[0])!=169 or int(octet_check_ip[1])!=254) and (0<=int(octet_check_ip[1])<=255) and (0<=int(octet_check_ip[2])<=255) and (0<=int(octet_check_ip[3])<=255)):
 				check=True
@@ -43,6 +44,30 @@ def ip_is_valid():
 		if check==False:
 			continue
 		elif check==True:
+			break
+	#Check IP reachability
+	print "\n* Checking IP reachability. Please wait...\n"
+	check2=False
+
+	while True:
+		for ip in ip_list:
+			ping_reply=subprocess.call(['ping','-c','2','-w','2','-q','-n',ip])
+			if ping_reply==0:
+				check2=True
+				continue
+			elif ping_reply==2:
+				print "\n* No response from device %s"%ip
+				check2=False
+				break
+			else:
+				print"\n Ping to the following device has FAILED:",ip
+				check2=False
+				break
+		if check2==False:
+			print"\n*Please re-check IP address list or device.\n"
+			ip_is_valid()
+		elif check2==True:
+			print"\n*All devices are reachable. Waiting for username/password file...\n"
 			break
 ip_is_valid()
 
